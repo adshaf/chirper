ad6d44e0bbafc893166a8efd3f30cb8b159179e2
Adam <76612324+adshaf@users.noreply.github.com>
2025-11-25 13:23:12 +1100

    our first cloud auto deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <x-slot:title>
         Welcome
     </x-slot:title>
-    
+
     <div class="max-w-2xl mx-auto">
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
@@ -10,6 +10,7 @@
                         <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
                         <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
                             sing (or chirp)!</p>
+                        <p class="mt-2 text-sm text-gray-600">Now live on the internet!!</p>
                     </div>
                 </div>
             </div>
